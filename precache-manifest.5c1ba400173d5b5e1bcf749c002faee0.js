7f0803f45ed5a483792bcb571144fc4682fe2b08
biio-front <bohwajung@naver.com>
2020-11-09 14:54:13 +0900

    Updates

---
+++ precache-manifest.5c1ba400173d5b5e1bcf749c002faee0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "654da73115459207ee4dd11bcabcde68",
+    "url": "/haagendaz_order_page/index.html"
+  },
+  {
+    "revision": "c3ab19f8ae757aa6c86e",
+    "url": "/haagendaz_order_page/static/css/main.e0d1b730.chunk.css"
+  },
+  {
+    "revision": "aeec50c08c7b8f390c4a",
+    "url": "/haagendaz_order_page/static/js/2.246cc52c.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/haagendaz_order_page/static/js/2.246cc52c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3ab19f8ae757aa6c86e",
+    "url": "/haagendaz_order_page/static/js/main.eba5c015.chunk.js"
+  },
+  {
+    "revision": "6a26bd194ffc4a9f1c0d",
+    "url": "/haagendaz_order_page/static/js/runtime-main.7bd99ca2.js"
+  }
+]);
