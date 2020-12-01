a11ad152564107a5f072fac215be0e7a18c939be
biio-front <bohwajung@naver.com>
2020-12-01 16:59:50 +0900

    add selecting all items btn in order

--- src/component/Order.js
+++ src/component/Order.js
@@ -9,17 +9,35 @@ function Order({ items, deleteItem_cart, deleteItem_LS }) {
   const [checked_id, setChecked_Id] = useState([]);
 
   // 체크한 아이템 state에 넣기
-  const checkForDelete = _id => {
+  const check = _id => {
     const _checked_id = [...checked_id];
     _checked_id.push(_id);
     setChecked_Id(_checked_id);
   };
-
   // 체크한 아이템 state에서 빼기
   const uncheck = _id => {
     const _checked_id = checked_id.filter(id => id !== _id);
     setChecked_Id(_checked_id);
   };
+
+  // 모든 아이템 체크하기
+  const checkAll = () => {
+    const checkbox = document.querySelectorAll('[name=chk_delete]');
+    checkbox.forEach(li => li.checked = true);
+    const _checked_id = [];
+    items.forEach(item => _checked_id.push(item.id));
+    setChecked_Id(_checked_id);
+  }
+  // 모든 아이템 체크 해제
+  const unCheckAll = () => {
+    const checkbox = document.querySelectorAll('[name=chk_delete]');
+    checkbox.forEach(li => li.checked = false);
+    setChecked_Id([]);
+  }
+  // 모든 아이템 체크하기 토글 버튼
+  const toggleCheckAll = e => {
+    e.target.checked ? checkAll() : unCheckAll();
+  }
 
   return (
     <article className="order-container">
@@ -32,7 +50,7 @@ function Order({ items, deleteItem_cart, deleteItem_LS }) {
                 key={item.id}
                 item={item}
                 index={index}
-                onCheck={id => checkForDelete(id)}
+                onCheck={id => check(id)}
                 unCheck={id => uncheck(id)}
               />
             ))
@@ -41,15 +59,21 @@ function Order({ items, deleteItem_cart, deleteItem_LS }) {
             )}
         </ul>
         {items.length > 0 ? (
-          <button
-            className="order-list__delete"
-            onClick={() => {
-              deleteItem_cart(checked_id);
-              deleteItem_LS(checked_id);
-            }}
-          >
-            선택한 상품 삭제하기
-          </button>
+          <div className="order-list__delete">
+            <input
+              type="checkbox"
+              onClick={e => toggleCheckAll(e)}
+            />
+            <button
+              className="order-list__delete-btn"
+              onClick={() => {
+                deleteItem_cart(checked_id);
+                deleteItem_LS(checked_id);
+              }}
+            >
+              선택한 상품 삭제하기
+            </button>
+          </div>
         ) : null}
       </div>
       <div className="total-order">
